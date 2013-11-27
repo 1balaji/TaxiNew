56e970afe703439c16218c70a3bed246a871e44d
liulin <liulin1987315@gmail.com>
2013-11-27 16:21:38 +0800

    get Message and can look the message

--- MainActivity/src/org/cug/driver/ShowMessageActivity.java
+++ MainActivity/src/org/cug/driver/ShowMessageActivity.java
@@ -1,5 +1,7 @@
 package org.cug.driver;
 
+import org.cug.util.Settings;
+import org.cug.util.Tools;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -25,6 +27,11 @@ public class ShowMessageActivity extends Activity {
 	private Button submitButton;
 	private Button cancelButton;
 
+	private String startNameStr;
+	private String endNameStr;
+	private String distanceStr;
+	private String usertimeStr;
+
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_showmessage);
@@ -36,27 +43,51 @@ public class ShowMessageActivity extends Activity {
 		submitButton = (Button) findViewById(R.id.button_showmeaasge_submit);
 		cancelButton = (Button) findViewById(R.id.button_showmeaasge_cancel);
 
+		if (!Settings.TESTMODE) {
+			startName.setText("南湖名都(A)区");
+			endName.setText("华中科技大学培训中心");
+			distance.setText("645982米");
+			usertime.setText("2012年01月12日 15时47分");
+
+		} else {
+
+			if (getSendMessage()) {
+				startName.setText(startNameStr);
+				endName.setText(endNameStr);
+				distance.setText(distanceStr);
+				usertime.setText(usertimeStr);
+			} else {
+
+				Tools.alert(getBaseContext(), "无新的推送信息！");
+			}
+		}
+
+	}
+
+	// 接收推送的自定义消息
+	private boolean getSendMessage() {
+		boolean isResult = false;
 		Intent intent = getIntent();
-		if (null != intent) {
-			Bundle bundle = getIntent().getExtras();
+		Bundle bundle = getIntent().getExtras();
+		if (null != bundle) {
+
 			String message = bundle.getString(JPushInterface.EXTRA_MESSAGE);
 			String extras = bundle.getString(JPushInterface.EXTRA_EXTRA);
 			if (!ExampleUtil.isEmpty(extras)) {
 				try {
 					JSONObject extraJson = new JSONObject(extras);
-					String valueStr = extraJson.getString("语文");
-					Log.d("MyReceiver", valueStr);
 					if (null != extraJson && extraJson.length() > 0) {
-
+						startNameStr = extraJson.getString("STARTADDRESS");
+						endNameStr = extraJson.getString("ENDADDRESS");
+						distanceStr = extraJson.getString("DISTANCE");
+						usertimeStr = extraJson.getString("NEEDTIME");
+						isResult = true;
 					}
 				} catch (JSONException e) {
-
 				}
-
 			}
-
 		}
-
+		return isResult;
 	}
 
 }
